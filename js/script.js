b52e201786ee0c1f23dc802b354d7e74d5999dd6
AlenaKatt <katt.elena@yandex.ru>
2020-07-07 20:34:04 +0300

    Проект Седона на проверку

--- js/script.js
+++ js/script.js
@@ -8,9 +8,15 @@ var form = bookingform.querySelector(".booking-form");
 
 var isStorageSupport = true;
 var storage = "";
+var storageAdult = "";
 
  try {
     storage = localStorage.getItem("children");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+  try {
+    storageAdult = localStorage.getItem("adult");
   } catch (err) {
     isStorageSupport = false;
   }
@@ -22,26 +28,28 @@ var storage = "";
     if (storage) {
         children.value = storage;
       }
+    if (storageAdult) {
+        adult.value = storageAdult;
+      } 
     datacoming.focus();
   });
 
   form.addEventListener("submit", function (evt) {
-      if (!datacoming.value || !datadeparture.value || !adult.value || !children.value) {
-          evt.preventDefault();
-        } else {
-          if (isStorageSupport) {
-          localStorage.setItem("children", children.value);
-          localStorage.setItem("adult", adult.value);
-          }
-        }
+    if (!datacoming.value || !datadeparture.value || !adult.value || !children.value) {
+        evt.preventDefault();
+      } else {
+        if (isStorageSupport) {
+        localStorage.setItem("children", children.value);
+        localStorage.setItem("adult", adult.value);
+      }
+    }
   });
 
   window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      if (bookingform.classList.contains("form-show")) {
+    if (evt.keyCode === 27 && bookingform.classList.contains("form-show")) {
         evt.preventDefault();
         bookingform.classList.remove("form-show");
-      }
+        bookingform.classList.add("form");
     }
   });
   
